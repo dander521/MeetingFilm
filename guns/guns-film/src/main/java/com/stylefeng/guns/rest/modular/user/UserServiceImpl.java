59be6423b7d93951869511beb70b0c7f49edeabc
Roger_Chen_max5211314 <rong123020990@126.com>
2019-07-06 15:16:57 +0800

    change api structure

--- guns/guns-film/src/main/java/com/stylefeng/guns/rest/modular/user/UserServiceImpl.java
+++ guns/guns-film/src/main/java/com/stylefeng/guns/rest/modular/user/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.alibaba.dubbo.config.annotation.Service;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.stylefeng.guns.api.user.UserAPI;
 import com.stylefeng.guns.api.user.vo.UserInfoModel;
+import com.stylefeng.guns.api.user.vo.UserModel;
 import com.stylefeng.guns.core.util.MD5Util;
 import com.stylefeng.guns.rest.common.persistence.dao.MoocUserTMapper;
 import com.stylefeng.guns.rest.common.persistence.model.MoocUserT;
